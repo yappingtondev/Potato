435c19cd7176a862a1554b68d60032d71d4a18a8
yappingtondev <zzamwater@gmail.com>
2026-07-18 04:51:53 +0500

    Create sas.lua

---
+++ sas.lua
@@ -0,0 +1,51 @@
+   local Workspace = game:GetService("Workspace")
+
+local function checkLineOfSight(cameraPart, character.Head)
+	local params = RaycastParams.new()
+	params.FilterType = Enum.RaycastFilterType.Exclude
+	params.FilterDescendantsInstances = {originPart.Parent}
+
+	local originPos = originPart.Position
+	local size = targetPart.Size / 2
+
+	-- Points we'll check
+	local points = {
+		targetPart.Position, -- center
+
+		targetPart.Position + Vector3.new(size.X,0,0),
+		targetPart.Position - Vector3.new(size.X,0,0),
+
+		targetPart.Position + Vector3.new(0,size.Y,0),
+		targetPart.Position - Vector3.new(0,size.Y,0),
+
+		targetPart.Position + Vector3.new(0,0,size.Z),
+		targetPart.Position - Vector3.new(0,0,size.Z),
+
+		-- corners
+		targetPart.Position + Vector3.new(size.X,size.Y,size.Z),
+		targetPart.Position + Vector3.new(-size.X,size.Y,size.Z),
+		targetPart.Position + Vector3.new(size.X,-size.Y,size.Z),
+		targetPart.Position + Vector3.new(-size.X,-size.Y,size.Z),
+
+		targetPart.Position + Vector3.new(size.X,size.Y,-size.Z),
+		targetPart.Position + Vector3.new(-size.X,size.Y,-size.Z),
+		targetPart.Position + Vector3.new(size.X,-size.Y,-size.Z),
+		targetPart.Position + Vector3.new(-size.X,-size.Y,-size.Z),
+	}
+
+	for _, targetPos in ipairs(points) do
+		local direction = targetPos - originPos
+
+		local result = Workspace:Raycast(originPos, direction, params)
+
+		-- Nothing hit = visible
+		if not result then
+			return true
+		end
+
+		-- Hit the character = visible
+		if result.Instance:IsDescendantOf(targetPart.Parent) then
+			return true
+    end
+  end
+    
